445dbbb1ac1632d27c47022e26dbd7dfef672627
Kyle Forkner <kyle.forkner@gmail.com>
2020-02-01 23:25:42 -0500

    hoping to fix build process by making it match example repo.  Removed node modules and updated src too.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. I was edited in the source_ja_change branch cause i can't spell - still! 
+          Edit <code>src/App.js</code> and save to reload. I was edited in the source_ja_change branch cause i can't spell - stillllllll! 
         </p>
         <a
           className="App-link"
